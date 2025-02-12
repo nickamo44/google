ee08bd5073efd1fb75f453f825d0f02d133ab4e9
nickamo44 <namouk8@gmail.com>
2025-02-11 20:42:56 -0800

    Search bar border and buttons outline

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let searchBarGroup = document.querySelector(".searchBarGroup");
 let searchBar = document.querySelector(".searchBar");
+const body =document.body;
 
 searchBarGroup.addEventListener("mouseover", function(){
-    this.style.backgroundColor = "rgb(180, 180, 180)";
+        this.style.backgroundColor = "rgb(180, 180, 180)";
 });
 
 searchBarGroup.addEventListener("mouseleave", function(){
-    if(searchBarGroup.clicked !== true){
         this.style.backgroundColor = "rgb(103, 103, 103)";
-    }
 });
 
 searchBarGroup.addEventListener("click", function(){
