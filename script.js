0ff735936db23b801104f5ac31942732ee58a601
nickamo44 <namouk8@gmail.com>
2025-02-11 17:51:37 -0800

    Search bar clickable area

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let searchBarGroup = document.querySelector(".searchBarGroup");
+let searchBar = document.querySelector(".searchBar");
 
 searchBarGroup.addEventListener("mouseover", function(){
-    this.style.backgroundColor = "rgb(180, 180, 180)"
+    this.style.backgroundColor = "rgb(180, 180, 180)";
 });
 
 searchBarGroup.addEventListener("mouseleave", function(){
-    this.style.backgroundColor = "rgb(103, 103, 103)"
+    if(searchBarGroup.clicked !== true){
+        this.style.backgroundColor = "rgb(103, 103, 103)";
+    }
 });
 
 searchBarGroup.addEventListener("click", function(){
-    this.style.backgroundColor = "rgb(71, 71, 71)"
+    this.style.backgroundColor = "rgb(71, 71, 71)";
+    searchBar.focus();
 });
